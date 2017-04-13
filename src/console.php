43ba595d308f187ac49dd257cb5174cf1907de70
Olivier Balais <obalais@gmail.com>
2017-04-13 21:03:00 +0200

    Add a cache date to improve sync perfs

--- src/console.php
+++ src/console.php
@@ -12,19 +12,41 @@ $console
     ->setDefinition([])
     ->setDescription('Store every storj contact along with IP geolocation in a cache file')
     ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+
+        $contactsCachePath = __DIR__.'/../var/contacts/contacts.json';
+        $existingContacts = [];
+        if (file_exists($contactsCachePath)) {
+            foreach (json_decode(file_get_contents($contactsCachePath), true) as $existingContact) {
+                $existingContact['lastUpdate'] = new DateTime();
+                $existingContacts[$existingContact['nodeID']] = $existingContact;
+            }
+        }
+
         $client = new Client();
 
         $hasAnotherPage = true;
         $page = 1;
         $contacts = [];
-        while ($hasAnotherPage && $page < 500) {
+        while ($hasAnotherPage && $page < 600) {
             $output->writeln(sprintf('Retrieve contact data from API (page: %d)', $page));
             $response = $client->get(sprintf('https://api.storj.io/contacts?connected=true&page=%d', $page++));
 
             $contacts = array_merge($contacts, json_decode($response->getBody(), true));
         }
 
-        $contacts = array_map(function ($contact) use ($output, $client) {
+        $cacheTime = new DateTime();
+        $cacheTime->modify('-1 day');
+
+        $contacts = array_map(function ($contact) use ($output, $client, $existingContacts, $cacheTime) {
+
+            if (isset($existingContacts[$contact['nodeID']])
+                && isset($existingContacts[$contact['nodeID']]['lastUpdate'])
+                && $existingContacts[$contact['nodeID']]['lastUpdate'] > $cacheTime) {
+
+                $output->writeln('Retrieve geolocation from cache');
+
+                return $existingContacts[$contact['nodeID']];
+            }
 
             $ipAddress = $contact['address'];
             $output->writeln(sprintf('Retrieve geolocation data for address: %s', $ipAddress));
@@ -35,6 +57,7 @@ $console
 
             $contact['lat'] = $geolocation['lat'];
             $contact['lng'] = $geolocation['lon'];
+            $contact['lastUpdate'] = new DateTime();
 
             usleep(500*1000); // to avoid API ban
 
@@ -42,7 +65,11 @@ $console
 
         }, $contacts);
 
-        file_put_contents(__DIR__.'/../var/contacts/contacts.json', json_encode($contacts));
+        $contacts = array_filter($contacts, function($contact) {
+            return isset($contact['lat']) && isset($contact['lng']);
+        });
+
+        file_put_contents($contactsCachePath, json_encode(array_values($contacts)));
     })
 ;
 
